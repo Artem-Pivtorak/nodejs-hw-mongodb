6b1f552a131124e5c36054c4b9778a0f64a0c641
Pivtorak Artem <mulbrry.digital@gmail.com>
2025-06-21 13:14:41 +0300

    hw-4

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -2,13 +2,46 @@ import createError from 'http-errors';
 import * as contactsService from '../services/contacts.js';
 
 export const getAllContacts = async (req, res) => {
-  const contacts = await contactsService.getAllContacts();
+  const {
+    page = 1,
+    perPage = 10,
+    sortBy = "name",
+    sortOrder = "asc",
+    type,
+    isFavourite
+  } = req.query;
+
+  const filter = {};
+  if (type) filter.contactType = type;
+  if (isFavourite !== undefined) filter.isFavourite = isFavourite === "true";
+
+  const totalItems = await contactsService.countContacts(filter);
+  const totalPages = Math.ceil(totalItems / perPage);
+  const skip = (page - 1) * perPage;
+
+  const contacts = await contactsService.getAllContacts({
+    filter,
+    sortBy,
+    sortOrder,
+    skip,
+    limit: Number(perPage),
+  });
+
   res.status(200).json({
     status: 200,
     message: "Successfully found contacts!",
-    data: contacts,
+    data: {
+      data: contacts,
+      page: Number(page),
+      perPage: Number(perPage),
+      totalItems,
+      totalPages,
+      hasPreviousPage: page > 1,
+      hasNextPage: page < totalPages,
+    },
   });
 };
+
 
 export const getContactById = async (req, res) => {
   const { contactId } = req.params;
